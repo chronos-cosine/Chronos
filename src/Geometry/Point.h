f37b91c3c80e1f2f5d4b180195cd13e533168ab6
chronos-cosine <43562769+chronos-cosine@users.noreply.github.com>
2018-10-25 12:09:37 +0200

    added geometry

---
+++ src/Geometry/Point.h
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Point.h
+ * Author: user
+ *
+ * Created on 25 October 2018, 10:32 AM
+ */
+
+#ifndef GEOMETRY_POINT_H
+#define GEOMETRY_POINT_H
+
+#include <iostream>
+
+namespace Geometry {
+   
+    struct Point {
+        double x;
+        double y;
+        
+        Point(const double& x, const double& y);
+        Point(const Point& point);
+        Point();
+        
+        Point operator+(const Point& rhs) const;
+        Point& operator+=(const Point& rhs);
+        Point operator-(const Point& rhs) const;
+        Point& operator-=(const Point& rhs);
+        bool operator==(const Point& rhs) const;
+        bool operator!=(const Point& rhs) const;
+        bool operator<(const Point& rhs) const;
+        bool operator>(const Point& rhs) const;
+        
+        friend std::ostream& operator<<(std::ostream& lhs, const Point& rhs);
+    };
+
+}
+
+#endif /* GEOMETRY_POINT_H */
+
