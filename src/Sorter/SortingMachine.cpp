af474a25b92d0947b8a6238bf8981b5e478a1207
chronos.cosine <chronos.cosine@gmail.com>
2018-11-19 21:10:33 +0200

    added sorting machine

--- src/Sorter/SortingMachine.cpp
+++ src/Sorter/SortingMachine.cpp
@@ -24,25 +24,93 @@ namespace Sorter {
     
     SortingMachine::SortingMachine(const std::vector<fs::path>& paths, 
                        const std::chrono::seconds& sleep_time) 
-      : Processors::ProcessorBase(sleep_time), 
-        collection(std::make_shared<
-            Collections::Concurrent::Queue<Sorter::Models::Job>>()) {
+      : collection(std::make_shared<
+            Collections::Concurrent::Queue<Sorter::Models::Job>>()),
+        __is_running(false),
+        __is_stopping(false) {
         initialise_producers(paths, sleep_time);
+        initialise_consumers();
     }
     
     void 
     SortingMachine::initialise_producers(const std::vector<fs::path>& paths, 
             const std::chrono::seconds& sleep_time) {
         for (auto& path: paths) {
-            File::Spooler<Sorter::Models::Job> producer(path, 
+            auto producer = std::make_shared<File::Spooler<Sorter::Models::Job>>(path, 
                     ".sjob", ".sdone", sleep_time, collection);
             producers.push_back(std::move(producer));
         }
     }
     
+    void 
+    SortingMachine::initialise_consumers() {
+        
+    }
+        
+    void 
+    SortingMachine::create_producer_threads() {
+        for (auto& producer: producers) {
+            std::thread thread(&Processors::IProcessor::start, producer);
+            thread.detach();
+            producer_threads.push_back(std::move(thread));
+        }
+    }
+    
+    void 
+    SortingMachine::create_consumer_threads() {
+        for (auto& consumer: consumers) {
+            std::thread thread(&Processors::IProcessor::start, consumer);
+            thread.detach();
+            consumer_threads.push_back(std::move(thread));
+        }
+    }
+    
+    void 
+    SortingMachine::stop_producer_threads() {
+        for (auto& producer: producers) {
+            producer->stop();
+        }
+    }
+    
+    void 
+    SortingMachine::stop_consumer_threads() {
+        for (auto& consumer: consumers) {
+            consumer->stop();
+        }
+    }
+
     bool 
-    SortingMachine::process() {
-        return false;
+    SortingMachine::start() {
+        std::lock_guard<std::mutex> lock(__mutex);
+        
+        if (__is_running || __is_stopping) { 
+            return false;
+        }
+        
+        __is_running = true;
+        create_producer_threads();
+        create_consumer_threads();
+        
+        return true;
+    }
+    
+    bool 
+    SortingMachine::stop() {
+        std::lock_guard<std::mutex> lock(__mutex);
+        
+        if (!__is_running || __is_stopping) {
+            return false;
+        }
+         
+        __is_stopping = true;
+        stop_producer_threads();
+        stop_consumer_threads();
+
+        producer_threads.clear();
+        consumer_threads.clear();
+
+        __is_stopping = false;
+        return true;
     }
     
 } /* namespace Sorter */
--- src/Sorter/SortingMachine.h
+++ src/Sorter/SortingMachine.h
@@ -19,6 +19,8 @@
 #include "Sorter/Models/Job.h"
 
 #include <experimental/filesystem>
+#include <mutex>
+#include <memory>
 #include <vector>
 #include <thread>
 
@@ -32,16 +34,25 @@ namespace Sorter {
         SortingMachine(const std::vector<fs::path>& paths, 
                        const std::chrono::seconds& sleep_time);
     public:
-        void start();
-        void stop();
+        bool start();
+        bool stop();
     private:
         void initialise_producers(const std::vector<fs::path>& paths,
                 const std::chrono::seconds& sleep_time);
         void initialise_consumers();
+        void create_producer_threads();
+        void create_consumer_threads();
+        void stop_producer_threads();
+        void stop_consumer_threads();
     private:
         std::shared_ptr<Collections::ICollection<Sorter::Models::Job>> collection;
-        std::vector<File::Spooler<Sorter::Models::Job>> producers;
+        std::vector<std::shared_ptr<Processors::IProcessor>> producers;
+        std::vector<std::shared_ptr<Processors::IProcessor>> consumers;
         std::vector<std::thread> producer_threads;
+        std::vector<std::thread> consumer_threads;
+        std::mutex __mutex;
+        bool __is_running;
+        bool __is_stopping;
         
     }; /* class SortingMachine */ 
     
