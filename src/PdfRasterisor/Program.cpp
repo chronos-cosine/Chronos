f98370d41914341caf864130959e3d7fa2377a5b
chronos.cosine@gmail.com <chronos.cosine@gmail.com>
2018-12-03 10:22:13 +0200

    use podofo linux build

--- src/PdfRasterisor/Program.cpp
+++ src/PdfRasterisor/Program.cpp
@@ -10,19 +10,51 @@
  *
  * Created on 03 December 2018, 7:58 AM
  */
-
+//https://github.com/NickNaso/PoDoFo
 //https://sourceforge.net/p/podofo/mailman/message/35788020/
-
-#include <cstdlib>
+ 
+#include <experimental/filesystem>
+#include <iostream>
 #include <podofo/podofo.h>
+#include <string>
 
-using namespace std;
+namespace fs = std::experimental::filesystem;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    std::cout << "Chronos-Pdf-Rasterisor..." << std::endl;
+    
+    std::string filename("/home/user/Documents/temp/pdf/De tijd/25/DeTijd_DeTijd_20181025_001.pdf");
+    
+    if (fs::exists(filename)) {
+        PoDoFo::PdfMemDocument document(filename.c_str());
+        for (int i = 0; i < document.GetPageCount(); ++i) {
+            std::cout << "Looping through page " << i << std::endl;
+            PoDoFo::PdfPage* page = document.GetPage(i);
+            PoDoFo::PdfContentsTokenizer tok(page);
+            const char* token = nullptr;
+            PoDoFo::PdfVariant var;
+            PoDoFo::EPdfContentsType type;
+            while (tok.ReadNext(type, token, var)) {
+                std::cout << token << std::endl;
+                switch (type) {
+                    case PoDoFo::ePdfContentsType_Keyword:
+                        // process token: it contains the current command
+                        //   pop from var stack as necessary
+                        break;
+                    case PoDoFo::ePdfContentsType_Variant:
+                        // process var: push it onto a stack
+                        break;
+                    default:
+                        // should not happen!
+                        break;
+                }
+            }
+        }
+    }
+    
     return 0;
 }
 
