9a7191333a2d83153bc75009a82628556c5de4cb
chronos.cosine@gmail.com <chronos.cosine@gmail.com>
2018-12-03 07:59:29 +0200

    use podofo linux build

---
+++ src/PdfRasterisor/Program.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Program.cpp
+ * Author: user
+ *
+ * Created on 03 December 2018, 7:58 AM
+ */
+
+//https://sourceforge.net/p/podofo/mailman/message/35788020/
+
+#include <cstdlib>
+#include <podofo/podofo.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
