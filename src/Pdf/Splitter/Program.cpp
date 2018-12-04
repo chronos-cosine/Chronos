5b291d85e2526b06caa3e16891e93fb5908570f8
chronos.cosine@gmail.com <chronos.cosine@gmail.com>
2018-12-04 07:08:27 +0200

    use podofo linux build

---
+++ src/Pdf/Splitter/Program.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Program.cpp
+ * Author: user
+ *
+ * Created on 04 December 2018, 7:07 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
